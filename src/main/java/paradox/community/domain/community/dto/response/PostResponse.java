897b92131aba24cfd0de6c4549423d335d056e15
Kr-Verified <dmschd0573@gmail.com>
2025-10-20 00:27:35 +0900

    fix: PostReponse 레코드로 고친 후, 좋아요와 조회수

--- src/main/java/paradox/community/domain/community/dto/response/PostResponse.java
+++ src/main/java/paradox/community/domain/community/dto/response/PostResponse.java
@@ -1,25 +1,35 @@
 package paradox.community.domain.community.dto.response;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import paradox.community.domain.community.model.Post;
 import paradox.community.domain.community.model.PostStatus;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class PostResponse {
-    private Long postId;
-    private String userId;
-    private Long characterId;
-    private String title;
-    private String body;
-    private PostStatus status;
-    private Boolean isBlind;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+public record PostResponse (
+    Long postId,
+    String userId,
+    Long characterId,
+    String title,
+    String body,
+    PostStatus status,
+    Boolean isBlind,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt,
+    Long viewsCount,
+    Long likesCount
+) {
+    public static PostResponse from(Post post) {
+        return new PostResponse(
+                post.getPostId(),
+                post.getUserId(),
+                post.getCharacterId(),
+                post.getTitle(),
+                post.getBody(),
+                post.getStatus(),
+                post.isBlind(),
+                post.getCreatedAt(),
+                post.getUpdatedAt(),
+                post.getViewsCount(),
+                post.getLikesCount()
+        );
+    }
 }
