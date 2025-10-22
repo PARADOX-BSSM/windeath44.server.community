00e7173ce9f65f146a3ab7ff8f92662202ea5515
Kr-Verified <dmschd0573@gmail.com>
2025-10-22 17:29:49 +0900

    feat: postcontroller finish

--- src/main/java/paradox/community/domain/community/controller/PostController.java
+++ src/main/java/paradox/community/domain/community/controller/PostController.java
@@ -1,13 +1,20 @@
 package paradox.community.domain.community.controller;
 
-import com.google.api.Page;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import paradox.community.domain.community.dto.request.PostCreateRequest;
 import paradox.community.domain.community.dto.request.PostSearchRequest;
+import paradox.community.domain.community.dto.request.PostUpdateRequest;
 import paradox.community.domain.community.dto.response.PostResponse;
 import paradox.community.domain.community.service.PostService;
 import paradox.community.domain.judgment.model.Judgment;
+import paradox.community.global.dto.ApiResponse;
+import paradox.community.global.util.HttpUtil;
 
 @RestController
 @RequestMapping("/communities/posts")
@@ -18,7 +25,36 @@ public class PostController {
 
     // 게시글 목록 조회
     @PostMapping
-    public ResponseEntity<Page<PostResponse>> getPosts(@RequestBody PostSearchRequest request) {
-        PostResponse post = postService.getPosts()
+    public ResponseEntity<ApiResponse<Page<PostResponse>>> getPosts(@RequestBody PostSearchRequest request, Pageable pageable) {
+        Page<PostResponse> posts = postService.getPosts(request, pageable);
+        return ResponseEntity.ok(HttpUtil.success("success post search", posts));
+    }
+
+    // 게시글 상세 조회
+    @GetMapping("/{postId}")
+    public ResponseEntity<ApiResponse<PostResponse>> getPost(@PathVariable Long postId, Pageable pageable) {
+        PostResponse post = postService.getPost(postId);
+        return ResponseEntity.ok(HttpUtil.success("success post search", post));
+    }
+
+    // 게시글 작성
+    @PostMapping
+    public ResponseEntity<ApiResponse<PostResponse>> createPost(@RequestHeader("user-id") String userId, @Valid @RequestBody PostCreateRequest request) {
+        PostResponse post = postService.createPost(userId, request);
+        return ResponseEntity.ok(HttpUtil.success("success post create", post));
+    }
+
+    // 게시글 수정
+    @PutMapping("/{postId}")
+    public ResponseEntity<ApiResponse<PostResponse>> updatePost(@PathVariable Long postId, @RequestHeader("user-id") String userId, @Valid @RequestBody PostUpdateRequest request) {
+        PostResponse post = postService.updatePost(postId, userId, request);
+        return ResponseEntity.ok(HttpUtil.success("success post update", post));
+    }
+
+    // 게시글 삭제
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<ApiResponse<Void>> deletePost(@PathVariable Long postId, @RequestHeader("user-id") String userId) {
+        postService.deletePost(postId, userId);
+        return ResponseEntity.ok(HttpUtil.success("success post delete"));
     }
 }
