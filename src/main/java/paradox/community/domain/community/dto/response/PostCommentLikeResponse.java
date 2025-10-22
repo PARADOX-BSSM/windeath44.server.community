bbb3fc3419e31900e73275a6fb2707969caa5614
ketarubot <robin09v@naver.com>
2025-10-22 22:20:37 +0900

    feat: response dto 수정

--- src/main/java/paradox/community/domain/community/dto/response/PostCommentLikeResponse.java
+++ src/main/java/paradox/community/domain/community/dto/response/PostCommentLikeResponse.java
@@ -1,10 +1,7 @@
 package paradox.community.domain.community.dto.response;
 
 public record PostCommentLikeResponse(
-        Long likeId,
         Long commentId,
         Long postId,
-        String userId,
-        Long likeCount,
-        Boolean isLiked
+        String userId
 ) {}
