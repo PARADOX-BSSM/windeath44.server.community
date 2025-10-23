205e18d4f91ff766643dc932ef46705c2cbf5048
Kr-Verified <dmschd0573@gmail.com>
2025-10-23 19:24:34 +0900

    fix: PostCommentLikeResponse add likeId

--- src/main/java/paradox/community/domain/community/dto/response/PostCommentLikeResponse.java
+++ src/main/java/paradox/community/domain/community/dto/response/PostCommentLikeResponse.java
@@ -3,11 +3,12 @@ package paradox.community.domain.community.dto.response;
 import paradox.community.domain.community.model.PostCommentLike;
 
 public record PostCommentLikeResponse(
+        Long likeId,
         Long commentId,
         Long postId,
         String userId
 ) {
     public static PostCommentLikeResponse from(PostCommentLike postCommentLike) {
-        return new PostCommentLikeResponse(postCommentLike.getPostCommentId(), postCommentLike.getPostId(), postCommentLike.getUserId());
+        return new PostCommentLikeResponse(postCommentLike.getLikeId(), postCommentLike.getPostCommentId(), postCommentLike.getPostId(), postCommentLike.getUserId());
     }
 }
