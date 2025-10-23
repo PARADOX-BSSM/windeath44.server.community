47f4a5042941e06a1504c9dee1bf57a5df877af9
Kr-Verified <dmschd0573@gmail.com>
2025-10-23 20:07:24 +0900

    fix: correction JudgmentCommentLike distinct variable

--- src/main/java/paradox/community/domain/judgment/model/JudgmentCommentLike.java
+++ src/main/java/paradox/community/domain/judgment/model/JudgmentCommentLike.java
@@ -23,8 +23,5 @@ public class JudgmentCommentLike {
     private String userId; // 좋아요를 누른 사용자
 
     @Column(name = "comment_id", nullable = false)
-    private Long commentId; // 댓글 아이디
-
-    @Column(name = "judgment_comment_id", nullable = false)
-    private Long judgmentCommentId; // 좋아요가 달린 댓글
+    private Long judgmentCommentId; // 댓글 아이디
 }
