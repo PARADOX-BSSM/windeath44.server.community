b3d291692fe94bf8f9e541efb621e438f4523911
Kr-Verified <dmschd0573@gmail.com>
2025-10-23 13:30:44 +0900

    fix: like v2

--- src/main/java/paradox/community/domain/community/service/PostLikeService.java
+++ src/main/java/paradox/community/domain/community/service/PostLikeService.java
@@ -14,6 +14,7 @@ public class PostLikeService {
 
     private final PostLikeRepository postLikeRepository;
 
+    // 좋아요
     @Transactional
     public PostLikeResponse addPostLike(Long postId, String userId) {
         if (postLikeRepository.existsByUserIdAndPostId(userId, postId)) {
@@ -29,10 +30,16 @@ public class PostLikeService {
         }
     }
 
+    // 좋아요 취소
     @Transactional
     public void removePostLike(Long postId, String userId) {
         if (!postLikeRepository.existsByUserIdAndPostId(userId, postId)) {
             postLikeRepository.deleteByUserIdAndPostId(userId, postId);
         }
+    }
+
+    // 좋아요 여부 판단
+    public Boolean isLiked(String userId, Long postId) {
+        return postLikeRepository.existsByUserIdAndPostId(userId, postId);
     }
 }
